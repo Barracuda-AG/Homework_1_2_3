2d01b07b61de05f68934c34054405e67ef92015b
Barracuda-AG <Nelson_73>
2020-11-04 21:54:01 +0200

    implementing business logic in Model class

--- src/Homework_2/Controller.java
+++ src/Homework_2/Controller.java
@@ -1,13 +1,10 @@
 package Homework_2;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Controller {
     private Model model;
     private View view;
-    private int minValue;
-    private int maxValue;
 
     Controller(Model model, View view) {
         this.model = model;
@@ -16,29 +13,17 @@ public class Controller {
 
     public void processUser() {
         boolean flag;
-
-        model.setRandomNumber(rand(0, 100));
+        view.printMessage(View.PROGRAM_GUESS);
+        model.setRandomNumber(model.rand(0, 100));
         Scanner scanner = new Scanner(System.in);
         do {
             model.setUserNumbers(inputValueWithScanner(scanner));
-            flag = checkUserNumber(model.getUserNumbers().get(model.getUserNumbers().size() - 1));
+            flag = model.checkUserNumber(model.getUserNumbers().get(model.getUserNumbers().size() - 1));
+            getMessage();
         } while (!flag);
+        view.printMessage(View.USER_GUESSED);
         view.printMessage(View.USER_TRIES);
         view.printMessage(model.getUserNumbers().toString());
-    }
-
-    public int rand(int min, int max) {
-        view.printMessage(View.PROGRAM_GUESS);
-        minValue = min;
-        maxValue = max;
-        return min + new Random().nextInt(max - min + 1);
-    }
-
-    public int rand() {
-        view.printMessage(View.PROGRAM_GUESS);
-        minValue = 0;
-        maxValue = 100;
-        return new Random().nextInt(100 + 1);
     }
 
     public int inputValueWithScanner(Scanner scanner) {
@@ -55,21 +40,14 @@ public class Controller {
         return userNumber;
     }
 
-    public boolean checkUserNumber(int userNumber) {
-
-        if (userNumber < model.getRandomNumber()) {
-            minValue = userNumber;
-            view.printMessage(View.NUMBER_IS_SMALLER);
-            view.printMessage("The range is from " + minValue + " to " + maxValue);
-            return false;
-        } else if (userNumber > model.getRandomNumber()) {
-            maxValue = userNumber;
+    public void getMessage() {
+        int userInput = model.getUserNumbers().get(model.getUserNumbers().size() - 1);
+        if (userInput > model.getRandomNumber()) {
             view.printMessage(View.NUMBER_IS_GREATER);
-            view.printMessage("The range is from " + minValue + " to " + maxValue);
-            return false;
-        } else {
-            view.printMessage(View.USER_GUESSED);
-            return true;
+            view.printMessage(View.RANGE + model.getMinValue() + View.AND + model.getMaxValue());
+        } else if (userInput < model.getRandomNumber()) {
+            view.printMessage(View.NUMBER_IS_SMALLER);
+            view.printMessage(View.RANGE + model.getMinValue() + View.AND + model.getMaxValue());
         }
     }
 }
